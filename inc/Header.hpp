9336f507a142a94632404b234a505f034287a5a4
fabbbiodc <fabiodicecca@gmail.com>
2025-08-05 14:34:29 +0200

    Testing

---
+++ inc/Header.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 14:04:30 by fdi-cecc          #+#    #+#             */
+/*   Updated: 2025/08/05 14:16:29 by fdi-cecc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include "Response.hpp"
+
+/* TODO building a basic header class
+to compose the right one to put at
+the beginning of the response*/
+
+class Header
+{
+	private:
+		Response _response;
+
+		Header(Header const &copy);
+		Header &operator=(Header const &copy);
+		Header();
+
+	public:
+};
+
+#endif
--- inc/Response.hpp
+++ inc/Response.hpp
@@ -6,7 +6,7 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 11:29:43 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2025/08/05 11:22:32 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2025/08/05 14:27:55 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,29 @@ back 200 */
 class Response
 {
 	private:
-	int					_clientFd;
-	std::string			_response;
-	std::string			_location;
-	std::string			_method;
-	std::ostringstream	_output;
-	std::string			_contentLength;
-	
-	Response(Response const &copy);
-	Response &operator=(Response const &copy);
-	
-	public:
-	Response();
-	~Response();
+		int _clientFd;
+		std::string _response;
+		std::string _location;
+		std::string _method;
+		std::ostringstream _output;
+		std::string _contentLength;
 
-	std::string	prepFile();
-	std::string	checkType();
-	void		setClientFd(int _clientFd);
-	void		setResponse(std::string response);
-	void		setContent(std::pair<std::string, std::string> fullPath, std::string method);
-	void		prepResponse();
-	void		sendResponse();
-	std::string	getResponse();
-	void		printRawResponse();
+		Response(Response const &copy);
+		Response &operator=(Response const &copy);
+
+	public:
+		Response();
+		~Response();
+
+		std::string prepFile();
+		std::string checkType();
+		void setClientFd(int _clientFd);
+		void setResponse(std::string response);
+		void setContent(std::pair<std::string, std::string> fullPath, std::string method);
+		void prepResponse();
+		void sendResponse();
+		std::string getResponse();
+		void printRawResponse();
 };
 
 #endif
